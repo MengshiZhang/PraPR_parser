a1a347da0c590c8f7b9e85854371ed32cdc637d0
Mengshi Zhang <mengshi.zhang@utexas.edu>
2021-02-24 23:26:46 -0800

    fixed a bug

--- PraPR_parser_v1_multi_core.py
+++ PraPR_parser_v1_multi_core.py
@@ -64,12 +64,17 @@ class PraPRParser:
                     modified_line_num = int(tag.split(":")[-1])
                     test_execution_list = parse_tests(testOrder)
                     failed_test_list = parse_tests(testFail)
+                    ordered_failed_test_list = []
+                    
+                    for test_i in test_execution_list:
+                        if test_i in failed_test_list:
+                            ordered_failed_test_list.append(test_i)
 
                     patch_dict[patch_id] = {
                         "method": method,
                         "line": modified_line_num,
                         "test_execution_list": test_execution_list,
-                        "failed_test_list": failed_test_list,
+                        "failed_test_list": ordered_failed_test_list,
                     }
 
                     patch_id += 1
@@ -137,16 +142,16 @@ class PraPRParser:
         merged_result_dict, id_method_mapping = self._merge_result(patch_dict, test_dict)
         
         # save full result
-        full_output_dir = os.path.join(self._output_dir, "full")
-        os.makedirs(full_output_dir, exist_ok=True)
-        full_output_filename = os.path.join(full_output_dir, "{}_{}.json".format(self.project, version))
+        # full_output_dir = os.path.join(self._output_dir, "full")
+        # os.makedirs(full_output_dir, exist_ok=True)
+        # full_output_filename = os.path.join(full_output_dir, "{}_{}.json".format(self.project, version))
 
-        with open(full_output_filename, 'w') as json_file:
-            json.dump({
-                "patch": merged_result_dict,
-                "method": id_method_mapping,
-                "test": test_dict,
-            }, json_file, indent=4)
+        # with open(full_output_filename, 'w') as json_file:
+        #     json.dump({
+        #         "patch": merged_result_dict,
+        #         "method": id_method_mapping,
+        #         "test": test_dict,
+        #     }, json_file, indent=4)
         
 
         # save partial result
@@ -173,7 +178,7 @@ class PraPRParser:
 
 
 if __name__ == "__main__":
-    data_dir = "/filesystem/patch_ranking/ProflPartialMatrix/python/data/prapr/yiling_data"
+    data_dir = "/filesystem/patch_ranking/ProflPartialMatrix/python/data/prapr/data"
     output_dir = "parsed_data"
     project = "Mockito"
     num_cores = 8
